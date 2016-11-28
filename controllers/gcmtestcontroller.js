f7efc61c8be0caae452a4ed12c0a1c6d98506d15
Nick D'hondt <nick.dhondt@student.howest.be>
2016-11-28 12:09:20 +0100

    fklsj

--- controllers/gcmtestcontroller.js
+++ controllers/gcmtestcontroller.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/", function (req, res) {
-    var body = JSON.stringify({condition: "'" + req.body.topic + "' in topics", notification:{title: req.body.from_id, body: req.body.announcement, sound:"default", priority: "high"}});
+    var body = JSON.stringify({condition: "'" + req.body.topic + "' in topics", priority: "high", notification:{title: req.body.from_id, body: req.body.announcement}});
 
     var options = {
         url: "https://fcm.googleapis.com/fcm/send",
